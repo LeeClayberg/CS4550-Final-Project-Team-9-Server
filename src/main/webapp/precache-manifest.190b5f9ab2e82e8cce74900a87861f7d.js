48a069be3ea1b5f34215774b9dfe79a21318a0af
Lee Clayberg <lee.clayberg@gmail.com>
2020-05-28 21:19:17 -0400

    first commit

---
+++ src/main/webapp/precache-manifest.190b5f9ab2e82e8cce74900a87861f7d.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "17c412cc036c117929e024bd31024f47",
+    "url": "/index.html"
+  },
+  {
+    "revision": "8fa9c52286262c98398a",
+    "url": "/static/css/2.a17a1ed7.chunk.css"
+  },
+  {
+    "revision": "a67c88a8e003f1ad34c2",
+    "url": "/static/css/main.30ac750d.chunk.css"
+  },
+  {
+    "revision": "8fa9c52286262c98398a",
+    "url": "/static/js/2.3c3eadb0.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.3c3eadb0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a67c88a8e003f1ad34c2",
+    "url": "/static/js/main.78dd62ba.chunk.js"
+  },
+  {
+    "revision": "ae7f77e7c61dcd85c5a4",
+    "url": "/static/js/runtime-main.0a69a1ac.js"
+  },
+  {
+    "revision": "651f7d9703ad500844818f51a452a94a",
+    "url": "/static/media/ComicStack.651f7d97.png"
+  },
+  {
+    "revision": "674f50d287a8c48dc19ba404d20fe713",
+    "url": "/static/media/fontawesome-webfont.674f50d2.eot"
+  },
+  {
+    "revision": "912ec66d7572ff821749319396470bde",
+    "url": "/static/media/fontawesome-webfont.912ec66d.svg"
+  },
+  {
+    "revision": "af7ae505a9eed503f8b8e6982036873e",
+    "url": "/static/media/fontawesome-webfont.af7ae505.woff2"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "/static/media/fontawesome-webfont.b06871f2.ttf"
+  },
+  {
+    "revision": "fee66e712a8a08eef5805a46892932ad",
+    "url": "/static/media/fontawesome-webfont.fee66e71.woff"
+  }
+]);
