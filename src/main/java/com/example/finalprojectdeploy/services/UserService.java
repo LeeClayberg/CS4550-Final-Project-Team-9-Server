1467025a3ef692d2c43ae49d8e820e0453bee67e
Lee Clayberg <lee.clayberg@gmail.com>
2020-06-09 13:23:39 -0400

    Fixed initial

--- src/main/java/com/example/finalprojectdeploy/services/UserService.java
+++ src/main/java/com/example/finalprojectdeploy/services/UserService.java
@@ -18,12 +18,8 @@ import java.util.List;
 @RestController
 @CrossOrigin(origins = "http://localhost:3000")
 public class UserService {
-  User user1 = new User("leeclayberg", "clayberg123", "admin", "2020-06-07");
   public List<String> history = new ArrayList<String>();
   public List<User> users = new ArrayList<User>();
-  {
-    users.add(user1);
-  }
 
   //Basic Operations
 
