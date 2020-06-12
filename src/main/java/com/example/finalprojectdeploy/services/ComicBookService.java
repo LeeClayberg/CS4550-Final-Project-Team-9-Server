38c3b939176f8abd96ee5c535e4a65e10eac4864
Lee Clayberg <lee.clayberg@gmail.com>
2020-06-11 23:53:59 -0400

    fixed controller

--- src/main/java/com/example/finalprojectdeploy/services/ComicBookService.java
+++ src/main/java/com/example/finalprojectdeploy/services/ComicBookService.java
@@ -4,6 +4,7 @@ import com.example.finalprojectdeploy.model.ComicBook;
 import com.example.finalprojectdeploy.repositories.ComicBookRepository;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -11,9 +12,12 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 
+@RestController
+@CrossOrigin(origins = "http://localhost:3000")
 public class ComicBookService {
 
   @Autowired
