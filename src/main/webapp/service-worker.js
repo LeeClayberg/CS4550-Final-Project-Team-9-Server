8eca43176e346526a01219a7ae65171e179d213a
Lee Clayberg <lee.clayberg@gmail.com>
2020-06-12 21:30:11 -0400

    Added react build 9

--- src/main/webapp/service-worker.js
+++ src/main/webapp/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.64a1bdffbde75ecbe17902a068df42c7.js"
+  "/precache-manifest.eaf208b3a1d6a976b73fa56b34e4496f.js"
 );
 
 self.addEventListener('message', (event) => {
